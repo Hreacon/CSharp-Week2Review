149376119c9621ca0b800bfc0fc93640954cd760
Nicholas Jensen and <student@epicodus.com>
2016-02-19 08:48:45 -0800

    Add final test to break it. Pass with flying colors!

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -15,7 +15,7 @@ namespace RepeatCounterNS.Objects
       int count = 0;
       // setup regex pattern
       string[] words = sentence.Split(' ');
-      Regex rgx = new Regex("[\\p{P}-]+");
+      Regex rgx = new Regex("[\\p{P}-\\^]+");
       foreach(string word in words)
       {
         if(rgx.Replace(word.ToLower(), "") == testWord.ToLower())
--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -40,5 +40,11 @@ namespace RepeatCounterNS
       RepeatCounter count = new RepeatCounter();
       Assert.Equal(1, count.CountRepeats("this wordy word.", "word"));
     }
+    [Fact]
+    public void CountRepeats_BreakTest()
+    {
+      RepeatCounter count = new RepeatCounter();
+      Assert.Equal(6, count.CountRepeats("'tEsT. sentence is tEsTing the ?tEsT' a bility of tEsTing the program that tEsTs for string [tEsT] occurances in tEsT; string tEsTy tEsT^ 'tEsT.", "test"));
+    }
   }
 }
